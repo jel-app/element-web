2034a7b32b8f98c475fbb0e0e06817663eea47c8
Greg Fodor <gfodor@gmail.com>
2021-04-06 12:01:47 -0700

    Re-add missing html

--- webpack.config.js
+++ webpack.config.js
@@ -370,18 +370,18 @@ module.exports = (env, argv) => {
             //}),
 
             // These are the static error pages for when the javascript env is *really unsupported*
-            //new HtmlWebpackPlugin({
-            //    template: "./src/vector/static/unable-to-load.html",
-            //    filename: "static/unable-to-load.html",
-            //    minify: argv.mode === "production",
-            //    chunks: []
-            //}),
-            //new HtmlWebpackPlugin({
-            //    template: "./src/vector/static/incompatible-browser.html",
-            //    filename: "static/incompatible-browser.html",
-            //    minify: argv.mode === "production",
-            //    chunks: []
-            //}),
+            new HtmlWebpackPlugin({
+                template: "./src/vector/static/unable-to-load.html",
+                filename: "static/unable-to-load.html",
+                minify: argv.mode === "production",
+                chunks: []
+            }),
+            new HtmlWebpackPlugin({
+                template: "./src/vector/static/incompatible-browser.html",
+                filename: "static/incompatible-browser.html",
+                minify: argv.mode === "production",
+                chunks: []
+            }),
 
             // This is the usercontent sandbox's entry point (separate for iframing)
             //new HtmlWebpackPlugin({
